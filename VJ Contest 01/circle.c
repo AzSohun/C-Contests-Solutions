6e702f1e8486c5bf4341b440446bc8135f33bd70
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-08 12:21:50 +0600

    Alternative Solution: Circle

---
+++ VJ Contest 01/circle.c
@@ -0,0 +1,59 @@
+// #define _USE_MATH_DEFINES
+// #include <stdio.h>
+// #include <math.h>
+
+// int main()
+// {
+
+//     double r;
+
+//     scanf("%lf", &r);
+
+//     double area = M_PI * r * r;
+//     double circumference = 2 * M_PI * r;
+
+//     printf("%lf %lf\n", area, circumference);
+
+//     return 0;
+// }
+
+// ------ Alternative
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    double r;
+
+    scanf("%lf", &r);
+
+    double pi = acos(-1);
+
+    double area = pi * pow(r, 2);
+    double circumference = 2 * pi * r;
+
+    printf("%lf %lf", area, circumference);
+
+    return 0;
+}
+
+// ------ !! wrong for using float
+// #define _USE_MATH_DEFINES
+// #include <stdio.h>
+// #include <math.h>
+
+// int main()
+// {
+
+//     float r;
+
+//     scanf("%f", &r);
+
+//     float area = M_PI * pow(r, 2);
+//     float circumference = 2 * M_PI * r;
+
+//     printf("%.6f %.6f\n", area, circumference);
+
+//     return 0;
+// }
