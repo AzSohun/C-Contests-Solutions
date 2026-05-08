c0722a2c63aaa3016ed180fda8f6903ff0f674b4
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-08 15:20:20 +0600

    Solved with java

---
+++ VJ Contest 01/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        try (Scanner input = new Scanner(System.in)) {
+
+            String x = input.next(); // ex: s = 123
+
+            // it convert char ascii value to int.
+            a = x.charAt(0) - '0'; // '49' - '48'
+            b = x.charAt(1) - '0'; // '50' - '48'
+            c = x.charAt(2) - '0'; // '51' - '48'
+
+            int sum = 111 * (a + b + c);
+
+            System.out.println(sum);
+
+        }
+    }
+}
