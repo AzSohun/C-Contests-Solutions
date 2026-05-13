1db6b9a268f60867305121a34ba1362fba67c307
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 01:18:50 +0600

    Solved: Thala for a reason

---
+++ VJ Contest 02/thala.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    if (n == 7)
+    {
+        printf("THALA");
+    }
+    else
+    {
+        printf("SADGE");
+    }
+
+    return 0;
+}
