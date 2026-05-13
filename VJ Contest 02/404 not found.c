3e0ef68e6f3b62b9b70e4d8c692b4b17168a3b65
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 01:43:11 +0600

    Found: 404 Not Found

---
+++ VJ Contest 02/404 not found.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+
+    scanf("%d", &x);
+
+    if (x == 404)
+    {
+        printf("NOT FOUND");
+    }
+    else
+    {
+        printf("FOUND");
+    }
+}
