43476d0c33f58784554ccba00f07cf7f2060bdc3
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 18:58:55 +0600

    Solved: Sport Section

---
+++ VJ Contest 02/sport-section.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+
+    scanf("%d", &x);
+
+    if (x >= 8 && x <= 10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
