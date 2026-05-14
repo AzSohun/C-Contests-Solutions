d84d875f08f1a26b0a0ca96e92a8d08b6c70c507
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 09:29:14 +0600

    Solved: Water-Park

---
+++ VJ Contest 02/water-park.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int w, h;
+
+    scanf("%d %d", &w, &h);
+
+    if (w >= 60 && h <= 130)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
