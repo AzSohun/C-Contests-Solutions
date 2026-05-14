63bf24cb166f8b658926aabd1dcc841e87d6a649
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 19:23:34 +0600

    Solved: Pizza Hunt

---
+++ VJ Contest 02/pizza-hunt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int msint()
+{
+
+    int x;
+
+    scanf("%d", &x);
+
+    if (x >= 6 && x <= 8)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
