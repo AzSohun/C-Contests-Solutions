6cb7ad80d15e2bc412b37ce94db711ab98d07255
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 19:05:16 +0600

    Solved: Air Quality Index

---
+++ VJ Contest 02/air-quality-index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+
+    scanf("%d", &x);
+
+    if (x < 100)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
