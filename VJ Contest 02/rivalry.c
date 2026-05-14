52ea0e0e7197a31d439e23c65744f39fe02ae93a
AzSohun <asaduzzamansohun5@gmail.com>
2026-05-14 11:32:09 +0600

    Solved: Rivalry

---
+++ VJ Contest 02/rivalry.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int r1, r2;
+    scanf("%d %d\n", &r1, &r2);
+
+    int d1, d2;
+    scanf("%d %d", &d1, &d2);
+
+    int dominator_rating = r1 + d1;
+    int everule_rating = r2 + d2;
+
+    if (dominator_rating > everule_rating)
+    {
+        printf("Dominator");
+    }
+    else
+    {
+        printf("Everule");
+    }
+
+    return 0;
+}
